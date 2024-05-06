b0dc4c680c32aede8c7478dd3b35d013ee3584dc
Philippe <philippe.lussou@gmail.com>
2024-05-06 15:27:22 +0200

    Update streamlit_app.py
    re-introduction parametre hauteur de poutre pour selection PAL

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,13 +15,14 @@ def select_cht(lg,ep,pose):
     selected.set_index('code',inplace=True)
     return selected
 
-def select_pal(lg):
+def select_pal(lg,ep):
     gamme_pal=pd.read_csv("data/gamme_pal.csv")
     sel_0=gamme_pal[gamme_pal['portee_min']<lg]
-    selected=sel_0[sel_0['portee_max']>=lg].drop(labels=['portee_min','portee_max'],axis=1)
-    selected.sort_values(by='h_poutre',inplace=True)
+    sel_1=sel_0[sel_0['portee_max']>=lg].drop(labels=['portee_min','portee_max'],axis=1)
+    sel_2=sel_1[sel_1['h_poutre']==ep]
+    selected = sel_2.rename({'p_ser':'charge admissible [kN/ml]',\
+                     'h_poutre':'hauteur poutre [cm]'},axis=1)
     selected.set_index('code',inplace=True)
-    selected.rename({'p_ser':'charge admissible [kN/ml]'},axis=1,inplace=True)
     return selected
 
 with st.sidebar:
@@ -35,16 +36,14 @@ with st.sidebar:
 if selected == "PAL":
     st.title("Poutre à longueur")
     lg = st.number_input("Portée ", min_value=200, max_value=700,step=50)
-#    ep = st.number_input("Hauteur poutre", min_value=16, max_value=24, step=4)
-
+    ep = st.number_input("Hauteur poutre", min_value=16, max_value=24, step=4)
     if st.button("Déterminer mon produit"):
-        st.dataframe(select_pal(lg))
+        st.dataframe(select_pal(lg,ep))
 
 if selected == "CHT":
     st.title("Chevêtre à longueur")
     pose = st.selectbox("Mode de pose",["Poutrelle/Poutrelle", "Poutrelle/Mur"])
     lg = st.number_input("Dimension trémie", min_value=90, max_value=450,step=10)
     ep = st.number_input("Epaisseur plancher", min_value=16, max_value=24, step=4)
-
     if st.button("Déterminer mon produit"):
         st.dataframe(select_cht(lg,ep,pose))
