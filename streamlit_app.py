a314648a7df6acca3c333a065e5a861a4b1f4052
Philippe <philippe.lussou@gmail.com>
2024-12-18 11:39:37 +0100

    add image in PAL menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ with st.sidebar:
 
 if selected == "PAL":
     st.title("Poutre à longueur")
+    st.image('media\\vue_enlong.jpg',width=500)
     lg = st.slider("Portée ", min_value=200, max_value=700,step=50)
     ep = st.number_input("Hauteur poutre", min_value=20, max_value=55, step=5)
     if st.button("Déterminer mon produit"):
